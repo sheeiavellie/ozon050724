355ebb1b5c974a80d62db5d1132ba64dadfafcda
Alexander V. Trotsky <josephlandmars@gmail.com>
2024-07-06 19:30:22 +0300

    fix: add port to .env

--- server.go
+++ server.go
@@ -7,12 +7,20 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
 	"github.com/sheeiavellie/ozon050724/graph"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	// env stuff
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
