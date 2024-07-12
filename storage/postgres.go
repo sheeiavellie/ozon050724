154109566f45ac0c8b0592c919b03bfcf0d63c38
Alexander V. Trotsky <josephlandmars@gmail.com>
2024-07-12 10:27:21 +0300

    feat: implement db initialization

--- storage/postgres.go
+++ storage/postgres.go
@@ -2,17 +2,53 @@ package storage
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"time"
 
 	"github.com/sheeiavellie/ozon050724/graph/model"
 )
 
+const postgresPingTimeout = 3 * time.Second
+
 type PostgresStorage struct {
+	db *sql.DB
 }
 
-func NewPostgresStorage() (*PostgresStorage, error) {
+func NewPostgresStorage(
+	ctx context.Context,
+	connStr string,
+) (*PostgresStorage, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("error opening postgres: %w", err)
+	}
+
+	ctxPing, cancel := context.WithTimeout(ctx, postgresPingTimeout)
+	defer cancel()
+
+	err = db.PingContext(ctxPing)
+	if err != nil {
+		return nil, fmt.Errorf("error pinging postgres: %w", err)
+	}
+
+	return &PostgresStorage{
+		db: db,
+	}, nil
 }
 
-func ClosePostgresStorageConnection() error {
+func (ps *PostgresStorage) Close() error {
+	err := ps.db.Close()
+	if err != nil {
+		return fmt.Errorf("error closing postgres: %w", err)
+	}
+	return nil
+}
+
+func (ps *PostgresStorage) Init(
+	ctx context.Context,
+) error {
+	return nil
 }
 
 func (ps *PostgresStorage) GetPostByID(
